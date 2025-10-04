a4ccd8e25e4bb7320694fd516300993fdc96a739
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2025-10-04 22:58:17 +0530

    variable declaration

---
+++ 01_BASICS/Variables.js
@@ -0,0 +1,24 @@
+const accountID= 14456
+let accountemail= "ishikasingh06122gmail.com"
+var accountPassword= "1452387"
+accountCity= "Jaipur"
+
+
+let accountState; //here variable is declared but value is not so it will be considerede as undefined
+
+//you can use semicolon at the end of each statement but it is optional in javascript
+
+
+//accountID= 36486 value of const variable can not be changed
+console.log(accountID);
+
+accountemail= "ishika123@gmail.com"
+accountPassword= "6487529"
+accountCity= "Mumbai"
+
+console.table([accountemail, accountPassword, accountCity])
+
+/*
+Prefer not to use var, use let and const instead, because var is function scoped and can create confusion.
+*/
+
